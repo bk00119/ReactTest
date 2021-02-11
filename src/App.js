05c2528cd84a02980980396d1c6092c87152b100
bk00119 <briankim00119@gmail.com>
2021-02-11 08:58:17 +0000

    third

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. LOL Worked!!!
+          Edit <code>src/App.js</code> and save to reload. LOL Worked!
         </p>
         <a
           className="App-link"
