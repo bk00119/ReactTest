d46a3afab3dd3c719bf37072b38ed1339a03bfab
root <root@ip-172-31-47-60.ap-northeast-2.compute.internal>
2021-02-12 07:04:37 +0000

    from ReactTest2 EC2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. LOL Worked!
-          lsdlfjasldfjlasjlfj!!
+          This is ReactTest!!
         </p>
         <a
           className="App-link"
